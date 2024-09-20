e43f70a4bdf8340e14f0c70c080217574b8e1b88
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-09-20 13:32:21 -0400

    Finished assigment

--- src/diceRolling.java
+++ src/diceRolling.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class diceRolling {
+import java.util.Random;
+
+public class diceRolling {
+    public static void main(String[] args) {
+
+        rollSixDice();
+
+    }
+
+    private static void rollSixDice() {
+        Random rand = new Random();
+        System.out.println("6 dices are rolled: ");
+        for(int i = 0; i <6; i++){
+            int roll = rand.nextInt(6) + 1;
+            System.out.println("Roll #" + (i + 1) + ": " + roll + " ");
+        }
+        System.out.println();
+    }
 }
